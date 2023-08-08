ec44313933db658e530672d8ab0aeffa63d8a21b
cttech.io <cttech.io@outlook.com>
2023-08-08 15:50:33 +0100

    Added TLDR update function & amended some wording

--- macOS-Updater.sh
+++ macOS-Updater.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+tldr=/opt/homebrew/bin/tldr
+
 ### Update Brew Packages
-echo "Updating brew packages"
+echo "UPDATING BREW PACKAGES"
 brew update && brew upgrade
+echo "UPDATED BREW PACKAGES"
+
+### Update TLDR cache
+if [[ -f "$tldr" ]];
+then
+    echo "UPDATING TLDR CACHE"
+    tldr -u
+    echo "UPDATED TLDR CACHE"
+fi
 
 ### Check for macOS updates
 read -p "Would you like to check for macOS udpates? (Y/N)" OS_UPDATE
     if [[ "$OS_UPDATE"]] =~ [Yy] ]]
     then
-        echo "Checking for macOS updates"
+        echo "CHECKING FOR 'macOS' UPDATES"
         softwareupdate --list
     else
+        echo "ALL UPDATES COMPLETE"
         exit
     fi
